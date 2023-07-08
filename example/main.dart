a1a8dbec8cc8a8d1c1b5a69185720782efeea9b0
Saed Khaled <saedkhaled96@gmail.com>
2023-07-09 01:04:15 +0300

    Add Example.

---
+++ example/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:web_video_player/player.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Web Video Player',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Media Collection Previewer'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            WebVideoPlayer(
+              url:
+                  'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+              autoPlay: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
